d74f1d099fd0cffb5f4523501cb1fcaf3cea5ad1
alex mai <cctvmm55@gmail.com>
2019-11-12 16:23:47 -0500

    Updates

---
+++ precache-manifest.52180fb50be5e9b5f87e8b48fbb35114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7e0059f331368a24fe74a409532725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fb70e4ca15c89dcc67",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6a29690e8396535d5aaf",
+    "url": "/static/js/2.c63dc231.chunk.js"
+  },
+  {
+    "revision": "62fb70e4ca15c89dcc67",
+    "url": "/static/js/main.b2982476.chunk.js"
+  },
+  {
+    "revision": "80fa4a6da20b3e84cd31",
+    "url": "/static/js/runtime-main.8be4ed17.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
