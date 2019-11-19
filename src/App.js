6161d785ef1acf6257e9b46186b8c5529b148047
alex mai <cctvmm55@gmail.com>
2019-11-18 22:01:37 -0500

    set default page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+
+import Blog from './Components/blog/blog.component';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Hello Friend
-      </Button>
+      <Blog/>
     </div>
   );
 }
---
+++ src/Components/blog/blog.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Blog = () => {
+  return(
+    <div>
+      It works
+    </div>
+  )
+};
+
+export default Blog;
